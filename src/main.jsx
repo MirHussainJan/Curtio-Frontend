d69075c13c9581947fb9a521854dcda46712be2c
Abdullah-AI122 <mrabdullahamjid33@gmail.com>
2026-05-22 02:34:00 +0500

    Initial commint/ Changes Fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from "@react-oauth/google"
-import env from '../Config/env.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={env.CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
